97ed7e4da8ec427c9b9add97f5eab805bcf9d312
agorchakovv <a.gorchakovv@gmail.com>
2022-11-29 18:50:02 +0600

    metod 3 element

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array = {"hello", "2", "world",":-)","sts","123asd","pasport","yes","P"};
+
+string[] Create3ElementArray(string[] array, int elementLenght = 3)
+{
+    int lengthNewArray = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i].Length <= elementLenght) lengthNewArray++;
+
+    string[] newArray = new string[lengthNewArray];
+
+    for(int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= elementLenght)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
